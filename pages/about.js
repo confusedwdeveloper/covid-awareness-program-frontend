d222d41fb511eba2729ba69aa4ec0d6c2958b459
Darshan Datta <darshandatta.10@gmail.com>
2021-04-21 00:52:23 +0530

    defined routes

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
-
-export default function About() {
+export default function AboutPage() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
-          Go to the main page
-        </Button>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      <h1>About</h1>
+      <p>
+        We are trying to bring awareness among general populace about covid-19,
+        how dangerous it is, and how to keep ourselves safe. Everyone is welcome
+        to join and plan events that contribute towards tackling covid-19 as
+        well as bringing awareness about vacinnation.
+      </p>
+      <button>Conditional Register</button>
+    </div>
   );
 }
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+  return (
+    <div>
+      <h1>MY Events</h1>
+    </div>
+  );
+}
